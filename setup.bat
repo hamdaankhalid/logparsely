ba5dddea414c2d05bd2c87f90decf53dd5d4ca18
Hamdaan Khalid <hkhalid@microsoft.com>
2023-12-01 00:38:32 +0530

    meow meow

--- setup.bat
+++ setup.bat
@@ -7,9 +7,9 @@ choco -V >> NUL
 IF %ErrorLevel% NEQ 0 @"%SystemRoot%\System32\WindowsPowerShell\v1.0\powershell.exe" -NoProfile -InputFormat None -ExecutionPolicy Bypass -Command " [System.Net.ServicePointManager]::SecurityProtocol = 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
 
 rustup --help >> NUL
-IF %ErrorLevel% NEQ 0 choco install rustup.install --version %rustupVersion% -y
+IF %ErrorLevel% NEQ 0 choco install rustup.install -y --version %rustupVersion%
 
 sqlite3 -version >> NULL
-IF %ErrorLevel% NEQ 0 choco install sqlitebrowser --version %sqliteVersion% -y
+IF %ErrorLevel% NEQ 0 choco install sqlitebrowser -y --version %sqliteVersion%
 
 refreshenv
